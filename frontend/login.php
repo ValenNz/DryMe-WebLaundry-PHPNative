f7c5fcbdb14fefd6d8479ba78ce9cca24ed91fe2
ValenNz <nuevalenra05@gmail.com>
2023-07-04 20:11:50 +0700

    update

--- frontend/login.php
+++ frontend/login.php
@@ -8,6 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Dry Me| Log in</title>
 
+  <link rel="icon" href="../image/logo_picture.png">
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
--- frontend/profile.php
+++ frontend/profile.php
@@ -1,10 +1,14 @@
-
+<?php 
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>DryMe | User Profile</title>
+
+  <link rel="icon" href="../image/logo_picture.png">
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
